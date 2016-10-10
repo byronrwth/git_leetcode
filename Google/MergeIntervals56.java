b048c51266c3ce1f1338c7e8d372b1a3b8e7b5d7
xi cheng <byroneagle@hotmail.com>
2016-10-10 23:22:42 +0200

    add merge interval 56

---
+++ Google/MergeIntervals56.java
@@ -0,0 +1,46 @@
+package Google;
+
+import java.util.List;
+/**
+ * Created by xicheng on 2016/10/6.
+ *
+ * Given a collection of intervals, merge all overlapping intervals.
+
+ Have you met this question in a real interview? Yes
+ Example
+ Given intervals => merged intervals:
+
+ [                     [
+ [1, 3],               [1, 6],
+ [2, 6],      =>       [8, 10],
+ [8, 10],              [15, 18]
+ [15, 18]            ]
+ ]
+ Challenge
+ O(n log n) time and O(1) extra space.
+ */
+public class MergeIntervals56 {
+    public class Interval {
+        int start, end;
+
+        Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    /**
+     * @param intervals, a collection of intervals
+     * @return: A new sorted interval list.
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+
+        
+        return null;
+    }
+
+
+
+
+}
+
