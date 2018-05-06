eea270cad74cd91e9eb7e165c7af69a1612514d6
xi cheng <byroneagle@gmail.com>
2018-05-06 08:24:12 +0200

    add tree iterative for any order template in py3

--- tree/BinaryTreePreorderTraversal144.py
+++ tree/BinaryTreePreorderTraversal144.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
 
 class Solution1:
     def preorderTraversal(self, root):
@@ -36,4 +36,32 @@ class Solution2:
                 res.append(node.val)
                 stack.append(node.right)
                 stack.append(node.left)
+        return res
+
+
+
+
+
+
+class Guide():
+    def __init__(self, ope, node):
+        #self.node = TreeNode.__init__(self)
+        self.ope = ope
+        self.node = node
+        
+class Solution3:
+    def preorderTraversal(self, root):
+        stack, res = [Guide(0, root)], []
+        while stack:
+            new_guide= stack.pop()
+
+            
+            if new_guide.node != None:
+                if new_guide.ope == 1:
+                    res.append(new_guide.node.val)
+                else:
+                
+                    stack.append(Guide(0, new_guide.node.right))
+                    stack.append(Guide(0, new_guide.node.left))
+                    stack.append(Guide(1, new_guide.node))
         return res
